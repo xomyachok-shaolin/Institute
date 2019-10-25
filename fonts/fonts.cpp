75207350f1a3655236e53a04c6e1f43efa188145
xomyachok_shaolin <xomyackok8@gmail.com>
2019-10-25 15:14:53 +0500

    Ready

--- fonts/fonts.cpp
+++ fonts/fonts.cpp
@@ -95,11 +95,77 @@ void fonts(void) {
 	cf.Flags = CF_SCREENFONTS | CF_EFFECTS;
 	if (ChooseFont(&cf) == TRUE) {
 		hdc = GetDC(hWndOut);
-		HFONT hf = CreateFontIndirect(cf.lpLogFont);
+		hf = CreateFontIndirect(cf.lpLogFont);
 		SelectObject(hdc, hf);
 		rgbCurrent = cf.rgbColors;
 		SetTextColor(hdc, rgbCurrent);
 		TextOut(hdc, 0, 80, "AbCdEf 012345 АБВГДЕ", 20);
 		ReleaseDC(hWndOut, hdc);
 	}
+
+	/* Создание шрифта */
+	// горизонтально
+	hf = CreateFont(16, 0, 0, 0, 0, 0, 0, 0, RUSSIAN_CHARSET, 0, 0, 0, 0, "Tahoma");
+	exhf = (HFONT)SelectObject(hdc, hf);
+	TextOut(hdc, 0, 120, "Horisontal", 10);
+	SelectObject(hdc, exhf);
+	DeleteObject(hf);
+	// вертикально
+	// SetGraphicsMode(CDC, GM_ADVANCED);
+	hf = CreateFont(16, 0, 900, 0, 0, 0, 0, 0, RUSSIAN_CHARSET, 0, 0, 0, 0, "Times New Roman");
+	exhf = (HFONT)SelectObject(hdc, hf);
+	TextOut(hdc, 0, 185, "Vertical", 8);
+	SelectObject(hdc, exhf);
+	DeleteObject(hf);
+	// с поворотом угла на 45град
+	hf = CreateFont(16, 0, -450, 0, 0, 0, 0, 0, RUSSIAN_CHARSET, 0, 0, 0, 0, "MS Sans Serif");
+	exhf = (HFONT)SelectObject(hdc, hf);
+	SetGraphicsMode(hdc, GM_ADVANCED);
+	TextOut(hdc, 0, 195, "With a rotation angle of 45 degrees", 35);
+	SelectObject(hdc, exhf);
+	DeleteObject(hf);
+	// с повернутыми буквами горизонт и верт
+	hf = CreateFont(16, 0, 0, 900, 0, 0, 0, 0, RUSSIAN_CHARSET, 0, 0, 0, 0, "Arial");
+	exhf = (HFONT)SelectObject(hdc, hf);
+	SetGraphicsMode(hdc, GM_ADVANCED);
+	TextOut(hdc, 210, 150, "With a rotation horizontal & vertical", 37);
+	SelectObject(hdc, exhf);
+	DeleteObject(hf);
+
+	hf = CreateFont(16, 0, 0, -900, 0, 0, 0, 0, RUSSIAN_CHARSET, 0, 0, 0, 0, "Arial");
+	exhf = (HFONT)SelectObject(hdc, hf);
+	SetGraphicsMode(hdc, GM_ADVANCED);
+	TextOut(hdc, 240, 170, "With a rotation horizontal & vertical", 37);
+	SelectObject(hdc, exhf);
+	DeleteObject(hf);
+
+	hf = CreateFont(16, 0, -900, -900, 0, 0, 0, 0, RUSSIAN_CHARSET, 0, 0, 0, 0, "Arial");
+	exhf = (HFONT)SelectObject(hdc, hf);
+	SetGraphicsMode(hdc, GM_ADVANCED);
+	TextOut(hdc, 170, 150, "With a rotation horizontal & vertical", 37);
+	SelectObject(hdc, exhf);
+	DeleteObject(hf);
+
+	hf = CreateFont(16, 0, -900, 900, 0, 0, 0, 0, RUSSIAN_CHARSET, 0, 0, 0, 0, "Arial");
+	exhf = (HFONT)SelectObject(hdc, hf);
+	SetGraphicsMode(hdc, GM_ADVANCED);
+	TextOut(hdc, 190, 150, "With a rotation horizontal & vertical", 37);
+	SelectObject(hdc, exhf);
+	DeleteObject(hf);
+	// с широкими и узкими символами
+	hf = CreateFont(16, 10, 0, 0, 0, 0, 0, 0, RUSSIAN_CHARSET, 0, 0, 0, 0, "Tahoma");
+	exhf = (HFONT)SelectObject(hdc, hf);
+	TextOut(hdc, 0, 400, "Witdh big width", 20);
+	SelectObject(hdc, exhf);
+	DeleteObject(hf);
+
+	TEXTMETRIC txm;
+	hf = CreateFont(16, 5, 0, 0, 0, 0, 0, 0, RUSSIAN_CHARSET, 0, 0, 0, 0, "Tahoma");
+	exhf = (HFONT)SelectObject(hdc, hf);
+	TextOut(hdc, 0, 420, "Width small width", 20);
+
+	GetTextMetrics(hdc, &txm);
+
+	SelectObject(hdc, exhf);
+	DeleteObject(hf);
 }
